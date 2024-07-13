a3230cc1829f98ba4812acb8e21123dd14d2d86d
eraycakinn <eraycakinn@hotmail.com>
2024-07-13 15:18:00 +0300

    .

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://os.eraycakin.com/';
+        iframe.src = 'http://37.27.27.121/';
         /**
          * Use dev server is query params are present
          *
